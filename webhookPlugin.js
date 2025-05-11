b9c869a6a874a07248014cddf8495d1872f2fba7
IPiyxl <104698846+IPiyxl@users.noreply.github.com>
2025-05-11 17:06:17 +0300

    Update webhookPlugin.js

--- webhookPlugin.js
+++ webhookPlugin.js
@@ -11,7 +11,7 @@ function sendToWebhook(entry) {
     ]
   };
 
-  fetch("/api/trimiteDiscord", {
+  fetch("/trimiteDiscord", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ embeds: [embed] })
